6b0ca653b42ccb79d84679033ad50a88fedf4b7a
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-02-24 12:27:07 -0800

    adds lint

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// sampled from https://github.com/oblador/angular-scroll/blob/master/gulpfile.js
+var gulp   = require('gulp');
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+
+var sources = [
+  'scrollAffix/scrollAffix.js',
+];
+
+
+gulp.task('lint', function() {
+  return gulp.src(sources)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('less', function () {
+  return gulp.src('scrollAffix/scrollAffix.less')
+    .pipe(less())
+    .pipe(gulp.dest('scrollAffix'));
+});
+
+
+gulp.task('test', ['lint']);
+gulp.task('default', ['test', 'less']);
