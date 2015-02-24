d0b18ac76950d0b880d8d59652203f3db0a273fe
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-02-24 12:14:44 -0800

    adds doc block

--- scrollAffix/scrollAffix.js
+++ scrollAffix/scrollAffix.js
@@ -5,7 +5,19 @@
 (function (angular, _, $) {
 
   /**
-   * like bootstrap affix, adds class `fixie` to pin elements
+   * like bootstrap affix, adds class `fixie` to pin elements when the
+   * scroll-affix-height is met.
+   * Usage:
+   *  <div class="section_nav_container" scroll-affix scroll-affix-height="110">
+   *      <div class="section_nav">
+   *          <a href="#building" offset="45" du-smooth-scroll du-scrollspy>Building</a>
+   *          <a href="#owner" offset="45" du-smooth-scroll du-scrollspy>Owner</a>
+   *          <a href="#audit" offset="45" du-smooth-scroll du-scrollspy>Audit</a>
+   *          <a href="#eems" offset="45" du-smooth-scroll du-scrollspy>EEMs</a>
+   *          <a href="#confirm" offset="45" du-smooth-scroll du-scrollspy>Confirm</a>
+   *          <a href="#comments" offset="45" du-smooth-scroll du-scrollspy><i class="fa fa-comments"></i> Comments</a>
+   *      </div>
+   *  </div>
    */
   angular.module('scrollAffix', [])
   .directive('scrollAffix', function () {
